eeb98bc5b826c34fa9d888193d64181190581658
punkundermyskin <p.nkundermyskin@gmail.com>
2019-08-29 01:15:15 +0300

    v1.0.0
    Added functions for working with TodoList - add, delete, strikeout.

---
+++ build/precache-manifest.8a8b854697ce4030ec48592dafd582ee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a637e72e4a6c3b2a826fe55c4c99f22f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5a253be64762fa0fa416",
+    "url": "/static/css/main.36fe29b4.chunk.css"
+  },
+  {
+    "revision": "79adef4dfcac61438c93",
+    "url": "/static/js/2.e1de7348.chunk.js"
+  },
+  {
+    "revision": "5a253be64762fa0fa416",
+    "url": "/static/js/main.307f6550.chunk.js"
+  },
+  {
+    "revision": "852bdc537e26ea9b00a2",
+    "url": "/static/js/runtime~main.cc526957.js"
+  }
+]);
